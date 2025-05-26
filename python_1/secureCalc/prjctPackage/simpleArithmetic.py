6a7e0a7ca422c549048b95febd02a4a39a7ff439
Zahra Shefa <zahra.shefa.1990@gmail.com>
2025-05-25 23:03:52 -0400

    Implemented multiplication table method, defined route in views, and styled the table and result page

--- python_1/secureCalc/prjctPackage/simpleArithmetic.py
+++ python_1/secureCalc/prjctPackage/simpleArithmetic.py
@@ -1,11 +1,16 @@
+import pandas as pd
 class SimpleArithmetic :
     def __init__(self):
         pass
 
     def multiplactionTable(self, number):
-        self.number=number
-        for i in range(1,11):
-            print(f'{i}*{self.number}={i*self.number}')
+        self.number=int(number)
+        multiplactionList=[]
+        for i in range(1,21):
+            multiplactionList.append((i,self.number,self.number*i))
+        resultPandasTbl=pd.DataFrame(multiplactionList,columns=['Multiplier','Number','Result'])
+        resultPdTohtmlTble=resultPandasTbl.to_html(classes='table table-bordered', index=False)
+        return resultPdTohtmlTble    
 
     def summation(self, numberList):
         self.list=numberList
