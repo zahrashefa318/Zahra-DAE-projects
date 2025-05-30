0d198df01b043ccb3f19a39aca8569bd9b49fb25
Zahra Shefa <zahra.shefa.1990@gmail.com>
2025-05-30 17:53:49 -0400

    Managing login and logout routes

---
+++ python_1/secureCalc/prjctPackage/utils.py
@@ -0,0 +1,12 @@
+from functools import wraps
+from flask import make_response
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return response
+    return no_cache
