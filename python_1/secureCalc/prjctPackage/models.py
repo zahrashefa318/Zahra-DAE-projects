63f1b08cc449b60d2b6ed476f0e2ad773b7549e1
Zahra Shefa <zahra.shefa.1990@gmail.com>
2025-07-10 14:13:25 -0400

    Squashed 'SecondProject/' changes from 6d721b5..4b5f68f
    4b5f68f ERDSecondDraft added.
    4ddb790 ER Diagram updated.
    
    git-subtree-dir: SecondProject
    git-subtree-split: 4b5f68f9056f9dfb83d9830720501e3b59f9d950

--- python_1/secureCalc/prjctPackage/models.py
+++
@@ -1,7 +0,0 @@
-from .dbExtension import db
-class userInfo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(100),nullable=False)
-    lastname=db.Column(db.String(100), nullable=False)
-    email=db.Column(db.String(100), nullable=False, unique=True)
-    password=db.Column(db.String(100), nullable=False)
