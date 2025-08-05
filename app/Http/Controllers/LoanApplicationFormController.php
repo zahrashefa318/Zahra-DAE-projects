947365b491ed37f6fd73952b3fe95c390af5f3a7
Zahra Shefa <zahra.shefa.1990@gmail.com>
2025-08-05 14:38:52 -0400

    Squashed 'SecondProject/' changes from f23d02d..9f5c617
    9f5c617 Loan Application Form blade completed.
    
    git-subtree-dir: SecondProject
    git-subtree-split: 9f5c61743950b512702606dd9263ce90e4febeee

--- app/Http/Controllers/LoanApplicationFormController.php
+++ app/Http/Controllers/LoanApplicationFormController.php
@@ -2,11 +2,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
 
 class LoanApplicationFormController extends Controller
 {
-    public function viewLoanApplicationForm()
-    {
+    public function viewLoanApplicationForm(Request $req)
+    {  $id=$req->query('id');
+         Customer::where('customer_id',$id)->update(['status'=>'pending']);
         return view('loanApplicationForm');
     }
 }
